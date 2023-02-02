f7cb7a4fcc487da78f8766280cd3b5ee4d28cdf0
Eight_Eggs <906324036@qq.com>
2023-02-02 14:26:57 +0800

    init

---
+++ src/main/java/io/github/eighteggs/fireworkyu/FireworkyuMain.java
@@ -0,0 +1,17 @@
+package io.github.eighteggs.fireworkyu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FireworkyuMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
